007190f0b345d15073196744a1ab462831bc0a50
Pravin kanna P <mp.pravinkanna@gmail.com>
2020-09-20 12:17:57 +0530

    added icons & fixed scrollbar bug

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon, fontAwesomeIcons } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Editor(props) {
   const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button type="button" className="expand-collapse-btn" onClick={() => setOpen((prevOpen) => !prevOpen)}>
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
